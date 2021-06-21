ccaccb28df5b80bb60fca95fe5c99dd103684ce1
Daniel Wimmer <danielwimmer08012003@gmail.com>
2021-06-21 11:36:51 +0200

    added project

---
+++ src/main/java/EmailHandler.java
@@ -0,0 +1,82 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailHandler {
+    private final String registrationText = "<h1>Registrierungsbestätigung</h1><p>Sie haben sich erfolgreich registriert und können unter folgendem Link einen Termin für Ihren kostenlosen COVID-19 Test buchen.</p><a href=\"http://www.stackoverflow.com/\" target=\"_blank\" style=\"padding: 0.6rem 1rem; background-color: #e32614; color: ffffff\">Termin buchen</a><p>-------------------------------------</p><p>Bundesministerium</p></p><p>Soziales, Gesundheit, Pflege</p><p>und Konsumentenschutz</p>";
+    private final String appointmentText = "<h1>Terminbestätigung</h1><p>Ihr Termin für den kostenlosen COVID-19 Test wurde gebucht.</p><p>Zum Test bitte mitbringen:</p><li>Gedruckten Laufzettel aus Ihrer Anmeldung oder die Laufzettel-Nummer</li><li>Ausweis</li><p style=\"font-weight: bold;\">Laufzettel-Nummer: plhLaufzettelNr</p><p style=\"font-weight: bold;\">Datum: plhDate</p><p style=\"font-weight: bold;\">Zeit: plhTime</p><p style=\"font-weight: bold;\">Ort: plhAddress - plhPostalCode plhCity</p><a href=\"http://www.stackoverflow.com/\" target=\"_blank\" style=\"padding: 0.6rem 1rem; background-color: #e32614; color: ffffff\">Termin anzeigen</a><p>-------------------------------------</p><p>Bundesministerium</p></p><p>Soziales, Gesundheit, Pflege</p><p>und Konsumentenschutz</p>";
+    private final String cancellationText = "<h1>Storno-Bestätigung</h1><p>Ihr Termin am TODO um TODO Uhr für den kostenlosen COVID-19 Test wurde storniert.</p><p>-------------------------------------</p><p>Bundesministerium</p></p><p>Soziales, Gesundheit, Pflege</p><p>und Konsumentenschutz</p>";
+
+    public void sendRegistrationEmail(String recipient) {
+        new EmailHandler().sendEmail(recipient, "Österreich testet - Bestätigung", registrationText);
+    }
+
+    public void sendAppointmentEmail(String recipient, String laufzettelNr, String date, String time, String address, String postalCode, String city) {
+        // Infos zu Termin von Firestore in Email einfuegen
+        String readyAppointmentText = appointmentText.replace("plhAddress", address);
+        readyAppointmentText = readyAppointmentText.replace("plhPostalCode", postalCode);
+        readyAppointmentText = readyAppointmentText.replace("plhCity", city);
+        readyAppointmentText = readyAppointmentText.replace("plhDate", date);
+        readyAppointmentText = readyAppointmentText.replace("plhTime", time);
+        readyAppointmentText = readyAppointmentText.replace("plhLaufzettelNr", laufzettelNr);
+
+        // Email senden
+        new EmailHandler().sendEmail(recipient, "Österreich testet - Bestätigung", readyAppointmentText);
+    }
+
+    public void sendCancelEmail(String recipient) {
+        new EmailHandler().sendEmail(recipient, "Österreich testet - Storno-Bestätigung", cancellationText);
+    }
+
+
+    private void sendEmail(String to, String subject, String content) {
+        String from = "ENTER EMAIL HERE";
+
+        // Assuming you are sending email from through gmails smtp
+        String host = "smtp.gmail.com";
+
+        // Get system properties
+        Properties properties = System.getProperties();
+
+        // Setup mail server
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "465");
+        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.auth", "true");
+
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        // Get the Session object.// and pass username and password
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, "ENTER PASSWORD"); // Unser Program kann emails derzeit von einem Gmail-Konto aus versenden
+            }
+        });
+
+        try {
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // Set Subject: header field
+            //message.setSubject("This is the Subject Line!");
+            message.setSubject(subject);
+
+
+            // Now set the actual message
+            message.setContent(content, "text/html");
+
+            // Send message
+            Transport.send(message);
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/MyFirestoreHandler.java
@@ -0,0 +1,159 @@
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.*;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class MyFirestoreHandler {
+
+    public static void main(String[] args) throws IOException {
+        FileInputStream service = null;
+        try {
+            service = new FileInputStream(new File(MyFirestoreHandler.class.getResource("/project.json").toURI()));
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(service))
+                .setDatabaseUrl("https://htlgkr-testet-default-rtdb.firebaseio.com/")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        Firestore firestore = FirestoreClient.getFirestore();
+
+        // Auf Aenderungen in Firestore reagieren
+        firestore.collection("Registrations")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirestoreException e) {
+                        if (e != null || queryDocumentSnapshots == null)
+                            return;
+
+                        // Datensaetze abfragen
+                        for (DocumentSnapshot doc : queryDocumentSnapshots) {
+
+                            // Datensatz information
+                            String email = (String) doc.get("email");
+                            boolean emailSent = (boolean) doc.get("emailSent");
+                            String selectedFacility = (String) doc.get("selectedFacility");
+                            boolean appointmentEmailSent = (boolean) doc.get("appointmentEmailSent");
+                            boolean isCanceled = (boolean) doc.get("isCanceled");
+                            boolean cancelEmailSent = (boolean) doc.get("cancelEmailSent");
+
+                            // Wenn registriert => registration-email senden
+                            if (!emailSent) {
+                                System.out.println("registration-email sent to: " + email + "\n");
+                                new EmailHandler().sendRegistrationEmail(email);
+
+                                // set emailSent == true
+                                Map<String, Object> update = new HashMap<>();
+                                update.put("emailSent", true);
+                                doc.getReference().set(update, SetOptions.merge());
+                            }
+
+                            // Wenn Termin gebucht => appointment-email senden
+                            if (selectedFacility != "" && !appointmentEmailSent) {
+//
+                                DocumentSnapshot docScreeningStation = getFirebaseDocument(firestore, "ScreeningStations", selectedFacility);
+
+                                if (docScreeningStation.exists()) {
+
+                                    // Termin Informationen
+                                    String laufzettelNr = (String) doc.get("laufzettelNr");
+                                    String address = (String) docScreeningStation.get("address");
+                                    int postalCode = docScreeningStation.getDouble("postalCode").intValue();
+                                    String city = (String) docScreeningStation.get("city");
+
+                                    String timeDayId = (String) doc.get("selectedTimeDay");
+                                    String date = getAppointmentDate(docScreeningStation, timeDayId);
+
+                                    String time = getAppointmentTime(docScreeningStation, timeDayId, doc);
+
+                                    System.out.println("LaufzettelNr: " + laufzettelNr);
+                                    System.out.println("Datum: " + date);
+                                    System.out.println("Zeit: " + time);
+                                    System.out.println("Address: " + address);
+                                    System.out.println("PostalCode: " + postalCode);
+                                    System.out.println("city: " + city);
+
+                                    new EmailHandler().sendAppointmentEmail(email, laufzettelNr, date, time, address, String.valueOf(postalCode), city);
+                                    System.out.println("appointment-email sent to: " + email + "\n");
+
+                                    // set appointmentEmailSent == true
+                                    Map<String, Object> update = new HashMap<>();
+                                    update.put("appointmentEmailSent", true);
+                                    doc.getReference().set(update, SetOptions.merge());
+                                } else {
+                                    System.err.println("Screening Station does not exist!");
+                                }
+                            }
+
+                            // Termin storniert => cancelEmail senden
+                            if (isCanceled && !cancelEmailSent) {
+                                System.out.println("cancel-email sent to: " + email + "\n");
+                                new EmailHandler().sendCancelEmail(email);
+
+                                // set cancelEmailSent == true
+                                Map<String, Object> update = new HashMap<>();
+                                update.put("cancelEmailSent", true);
+                                doc.getReference().set(update, SetOptions.merge());
+                            }
+                        }
+                    }
+                });
+        while (true) ;
+    }
+
+    private static DocumentSnapshot getFirebaseDocument(Firestore firestore, String collectionName, String documentName) {
+        DocumentReference docRef = firestore.collection(collectionName).document(documentName);
+
+        // asynchronously retrieve the document
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+
+        // future.get() blocks on response
+        DocumentSnapshot document = null;
+        try {
+            document = future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return document;
+    }
+
+    private static String getAppointmentDate(DocumentSnapshot docScreeningStation, String timeDayId) {
+        DocumentReference timeDayFuture = docScreeningStation.getReference().collection("timeDays").document(timeDayId);
+        ApiFuture<DocumentSnapshot> timeDaySnapshot = timeDayFuture.get();
+        try {
+            DocumentSnapshot timeDay = timeDaySnapshot.get();
+            return (String) timeDay.get("date");
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } catch (ExecutionException ex) {
+            ex.printStackTrace();
+        }
+
+        return "Date not found!";
+    }
+
+    private static String getAppointmentTime(DocumentSnapshot docScreeningStation, String timeDayId, DocumentSnapshot doc) {
+        ApiFuture<DocumentSnapshot> timeSlotFuture = docScreeningStation.getReference().collection("timeDays").document(timeDayId).collection("slots").document((String) doc.get("selectedTimeSlot")).get();
+        try {
+            DocumentSnapshot timeSlot = timeSlotFuture.get();
+            return (String) timeSlot.get("time");
+        } catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
+        }
+        return "Time not found!";
+    }
+}
